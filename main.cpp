e4cbd4f8a7c089ee3835591ce7e4d07e028e2569
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2025-01-19 20:34:18 +0100

    refactor(app): Change from C dir handling to C++

--- main.cpp
+++ main.cpp
@@ -5,33 +5,30 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <filesystem>
+#include <algorithm>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-    DIR *d;
-    struct dirent *ent;
+
     int count = 0;
     int yMax, xMax;
 
 
     initscr();
     getmaxyx(stdscr, yMax, xMax);
+    std::vector<std::string> dirFiles;
 
-    keypad(stdscr, TRUE);
-    noecho();
     curs_set(0);
     WINDOW *win = newwin(yMax/2, xMax/2, yMax/4, xMax/4);
+    keypad(win, TRUE);
+    noecho();
+    for (const auto& entry : std::filesystem::directory_iterator(".")){
+        dirFiles.push_back(entry.path().filename().string());
+    }
+    std::sort(dirFiles.begin(), dirFiles.end());
     box(win, 0, 0);
-    mvwprintw(win, 0, 2, "Directory\n");
-    if ((d = opendir(".")) == nullptr){
-        std::cerr << "Could not open directory!";
-        return 1;
-    }
-    while ((ent = readdir(d)) != nullptr){
-        count += 1;
-        wprintw(win, "%i. %s\n", count, ent->d_name);
-    }
 
-
+    mvwprintw(win, 0, 2, "Directory");
 
 
     int ch;
