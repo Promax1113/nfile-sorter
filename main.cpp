b6944bbc901b4d2ecc323c52c83d41ae5804fad8
promax1113 <67706765+Promax1113@users.noreply.github.com>
2025-02-11 21:37:04 +0100

    feat: Started the page system and input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "pageSystem/pageSystem.h"
 #include <algorithm>
+#include <cstdio>
 #include <dirent.h>
 #include <filesystem>
 #include <iostream>
@@ -50,9 +51,12 @@ PageSystem::directoryFiles_t getDirectoryEntriesSorted() {
   std::string filepath = std::filesystem::current_path().string();
   PageSystem::directoryFiles_t dirFiles;
   for (const auto &entry : std::filesystem::directory_iterator(filepath)) {
-    if (std::filesystem::is_directory(entry) || entry.path().filename().string() == "..")
+    if (std::filesystem::is_directory(entry) ||
+        entry.path().filename().string() == "..") {
+      wprintw(stdscr, "%s", entry.path().filename().c_str());
       dirFiles.push_back(
           std::make_pair(entry.path().filename().string(), entry));
+    }
   }
   sort(dirFiles.begin(), dirFiles.end());
   return dirFiles;
@@ -64,8 +68,9 @@ void displayDirectory(const std::string filepath) {
 
   std::filesystem::current_path(filepath);
   std::string curr = std::filesystem::current_path().string();
-  std::vector<std::string> leftItems = {"Directory", std::filesystem::current_path().string()};
-  std::vector<std::string> rightItems = {"v0.0.1","nfile"};
+  std::vector<std::string> leftItems = {
+      "Directory", std::filesystem::current_path().string()};
+  std::vector<std::string> rightItems = {"v0.0.1", "nfile"};
 
   PageSystem::directoryFiles_t files = getDirectoryEntriesSorted();
 
@@ -82,15 +87,15 @@ void displayDirectory(const std::string filepath) {
   PageSystem::PageInfo Pages(10, 0);
   Toolbar stdscr_toolbar(stdscr, 2, 3, 0, leftItems, rightItems);
   PageSystem::directoryFiles_t currPageFiles = Pages.getPageItems(files);
+  int currentItem = 0;
   int count = 0;
-  int offset = 1;
+  int offset = 2;
 
   for (const auto &ent : currPageFiles) {
     mvwprintw(stdscr, offset, 4, "%i. %s/", count, ent.first.c_str());
     offset += 1;
     count += 1;
   }
-
 
   std::vector<std::string> dirFiles;
 
@@ -104,8 +109,22 @@ void displayDirectory(const std::string filepath) {
   stdscr_toolbar.printToolbar(yMax, xMax);
 
   int ch;
+
   while ((ch = wgetch(stdscr)) != 'q' && ch != 'Q') {
-    continue;
+
+    switch (ch) {
+    case KEY_UP:
+      if (currentItem + 1 == currPageFiles.size())
+        currentItem -= 1;
+    case KEY_DOWN:
+      currentItem += 1;
+    case KEY_ENTER:
+      if (currPageFiles.at(currentItem).first == "..") {
+        displayDirectory(filepath + "/..");
+      }
+    default:
+      continue;
+    }
   }
   endwin();
 }
@@ -118,6 +137,5 @@ int main(int argc, char *argv[]) {
   } else {
     std::cout << "No arg. defaulting to '.'\n";
   }
-
   displayDirectory(filepath);
 }
